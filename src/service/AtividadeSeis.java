b999136ac2ee3e8a58cc54fdd37f6822b028ccae
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-07-27 09:50:08 -0300

    Atividade Seis

--- src/service/AtividadeSeis.java
+++ src/service/AtividadeSeis.java
@@ -1,4 +1,26 @@
 package service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class AtividadeSeis {
+    public void executar(){
+        Map<Integer, String> clientes = new HashMap<>();
+        clientes.put(1, "Maria");
+        clientes.put(2, "Marcos");
+        clientes.put(3, "Ana");
+        clientes.put(4, "Joana");
+        clientes.put(5, "Karen");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o ID do cliente: ");
+        int id = scanner.nextInt();
+
+        if (clientes.containsKey(id)){
+            System.out.println("O nome do cliente com ID " + id + " é: " + clientes.get(id));
+        } else {
+            System.out.println(" Cliente com ID " + id + " não encontrado.");
+        }
+    }
 }
