9782170757542aa6351418c5a1cd335592346182
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-07-28 11:27:15 -0300

    Atividade Sete

--- src/service/AtividadeSete.java
+++ src/service/AtividadeSete.java
@@ -1,4 +1,12 @@
 package service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AtividadeSete {
+    public void executar(){
+        List<String> funcionarios = List.of("Ana", "Bruno", "Carlos", "Amanda", "Alice", "Daniel", "Caroline");
+        List<String> nomesCurtos = funcionarios.stream().filter(f -> f.length() <= 5).collect(Collectors.toList());
+        System.out.println(nomesCurtos);
+    }
 }
