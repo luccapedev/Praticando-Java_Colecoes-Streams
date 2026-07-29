d4e29b82506dccc30ba0f913bddb79620c1510a0
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-07-29 09:16:43 -0300

    Atividade Oito

--- src/service/AtividadeOito.java
+++ src/service/AtividadeOito.java
@@ -1,4 +1,15 @@
 package service;
 
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class AtividadeOito {
+    public void executar(){
+        List<Integer> numeros = List.of(2, 3, 5, 7, 11);
+        List<Integer> numerosQuadrados = numeros.stream().map(f -> f * f).collect(Collectors.toList());
+
+        System.out.println("Quadrados dos números: " + numerosQuadrados);
+    }
 }
