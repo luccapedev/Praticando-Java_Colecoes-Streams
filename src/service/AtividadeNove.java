4a12cf8e841acf159dc4e19461a3b8c9f71cc5c1
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-08-03 17:50:11 -0300

    Atividade Nove

--- src/service/AtividadeNove.java
+++ src/service/AtividadeNove.java
@@ -7,10 +7,10 @@ public class AtividadeNove {
         List<Double> precosProdutos = List.of(29.99, 49.50, 15.75, 99.99);
         double totalGasto = precosProdutos.stream().reduce(0.0, Double::sum);
 
-        double imposto = totalGasto * 0.08;
+        double imposto = totalGasto * 0.08; 
         double totalComImposto = totalGasto + imposto;
 
         System.out.println("Valor total antes do imposto: R$" + String.format("%.2f", totalGasto));
-        System.out.println("Valor total com imposto de 8%: R$" + String.format("%.2f", totalComImposto)); 
+        System.out.println("Valor total com imposto de 8%: R$" + String.format("%.2f", totalComImposto));
     }
 }
