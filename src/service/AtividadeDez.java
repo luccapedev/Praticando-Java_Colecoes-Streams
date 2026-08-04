8528618cb1e05427463b494187f13cb4befb751a
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-08-04 10:13:07 -0300

    Atividade Dez

--- src/service/AtividadeDez.java
+++ src/service/AtividadeDez.java
@@ -1,4 +1,16 @@
 package service;
 
+import java.util.List;
+
 public class AtividadeDez {
+    public void executar(){
+        List<Double> notas = List.of(7.5, 8.0, 6.5, 9.0, 10.0);
+        double mediaNotas = notas.stream().reduce(0.0, Double::sum) / notas.size();
+        double maiorNota = notas.stream().reduce(Double::max).orElse(0.0);
+        double menorNota = notas.stream().reduce(Double::min).orElse(0.0);
+
+        System.out.println("A média das notas é: " + String.format("%.1f", mediaNotas));
+        System.out.println("A maior nota é: " + String.format("%.1f", maiorNota));
+        System.out.println("A menor nota é: " + String.format("%.1f", menorNota));
+    }
 }
--- src/service/AtividadeNove.java
+++ src/service/AtividadeNove.java
@@ -7,7 +7,7 @@ public class AtividadeNove {
         List<Double> precosProdutos = List.of(29.99, 49.50, 15.75, 99.99);
         double totalGasto = precosProdutos.stream().reduce(0.0, Double::sum);
 
-        double imposto = totalGasto * 0.08; 
+        double imposto = totalGasto * 0.08;
         double totalComImposto = totalGasto + imposto;
 
         System.out.println("Valor total antes do imposto: R$" + String.format("%.2f", totalGasto));
